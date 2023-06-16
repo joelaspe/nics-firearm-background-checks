625934a789e9c9361e7f0e57e5a1f032ea38def7
joelaspe <joe.laspe@gmail.com>
2023-06-16 16:35:53 -0700

    Added some data checking on POST route. Created a function called checkValidInputKey(input) that checks the keys of the input json object and compares to a valid list of keys.

--- server.js
+++ server.js
@@ -60,17 +60,42 @@ app.get('/checks/:state_abbv/:id', async (req, res) => {
 /******** POST ONE CHECK, requires optional body data */
 app.post('/checks',  async (req, res) => {
     const body = req.body;
-    try {
-        const result = await pool.query('INSERT INTO checks (month_year, state_id, permit, permit_recheck, handgun, long_gun, other, multiple, admin, prepawn_handgun, prepawn_long_gun, prepawn_other, redemption_handgun, redemption_long_gun, redemption_other, returned_handgun, returned_long_gun, returned_other, rentals_handgun, rentals_long_gun, private_sale_handgun, private_sale_long_gun, private_sale_other, return_to_seller_handgun,return_to_seller_long_gun, return_to_seller_other, totals) VALUES ($1, (SELECT id FROM states WHERE name = $2), $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27) RETURNING *', [body.month_year, body.name, body.permit, body.permit_recheck, body.handgun, body.long_gun, body.other, body.multiple, body.admin, body.prepawn_handgun, body.prepawn_long_gun, body.prepawn_other, body.redemption_handgun, body.redemption_long_gun, body.redemption_other, body.returned_handgun, body.returned_long_gun, body.returned_other, body.rentals_handgun, body.rentals_long_gun, body.private_sale_handgun, body.private_sale_long_gun, body.private_sale_other, body.return_to_seller_handgun, body.return_to_seller_long_gun, body.return_to_seller_other, body.totals]);
-        res.status(201).json(result.rows[0]);
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
+    if(!checkValidInputKey(body)) {
+        res.status(400).send('Bad Data, unable to create record. Ensure data is in json format. Example: {"month_year":"2023-06-01", "name":"Alabama", "permit":11111, "permit_recheck":2222, "handgun":3333, "long_gun":4444, "other":5555, "multiple":6666, "admin":7777, "prepawn_handgun":8888, "prepawn_long_gun":9999, "prepawn_other":12323, "redemption_handgun":3445, "redemption_long_gun":445667, "redemption_other":55678, "returned_handgun":78999, "returned_long_gun":11111, "returned_other":22222, "rentals_handgun":33333, "rentals_long_gun":44444, "private_sale_handgun":55555, "private_sale_long_gun":66666, "private_sale_other":77777, "return_to_seller_handgun":88888,"return_to_seller_long_gun":9999, "return_to_seller_other":12345,"totals":999999}');
+    
     }
-})
+    else {
+        try {
+            const result = await pool.query('INSERT INTO checks (month_year, state_id, permit, permit_recheck, handgun, long_gun, other, multiple, admin, prepawn_handgun, prepawn_long_gun, prepawn_other, redemption_handgun, redemption_long_gun, redemption_other, returned_handgun, returned_long_gun, returned_other, rentals_handgun, rentals_long_gun, private_sale_handgun, private_sale_long_gun, private_sale_other, return_to_seller_handgun,return_to_seller_long_gun, return_to_seller_other, totals) VALUES ($1, (SELECT id FROM states WHERE name = $2), $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20, $21, $22, $23, $24, $25, $26, $27) RETURNING *', [body.month_year, body.name, body.permit, body.permit_recheck, body.handgun, body.long_gun, body.other, body.multiple, body.admin, body.prepawn_handgun, body.prepawn_long_gun, body.prepawn_other, body.redemption_handgun, body.redemption_long_gun, body.redemption_other, body.returned_handgun, body.returned_long_gun, body.returned_other, body.rentals_handgun, body.rentals_long_gun, body.private_sale_handgun, body.private_sale_long_gun, body.private_sale_other, body.return_to_seller_handgun, body.return_to_seller_long_gun, body.return_to_seller_other, body.totals]);
+            
+            res.status(201).json(result.rows[0]);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error, unable to create record. Ensure data is in json format. Example: {"month_year":"2023-06-01", "name":"Alabama", "permit":11111, "permit_recheck":2222, "handgun":3333, "long_gun":4444, "other":5555, "multiple":6666, "admin":7777, "prepawn_handgun":8888, "prepawn_long_gun":9999, "prepawn_other":12323, "redemption_handgun":3445, "redemption_long_gun":445667, "redemption_other":55678, "returned_handgun":78999, "returned_long_gun":11111, "returned_other":22222, "rentals_handgun":33333, "rentals_long_gun":44444, "private_sale_handgun":55555, "private_sale_long_gun":66666, "private_sale_other":77777, "return_to_seller_handgun":88888,"return_to_seller_long_gun":9999, "return_to_seller_other":12345,"totals":999999}');
+        }
+    }
+});
 
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started, listening on port: ${process.env.PORT}`);
 })
 
+
+/**** Keyname data validation function 
+ **** Returns false if a key in the object is not a valid one, otherwise returns true
+*****/
+function checkValidInputKey(input) {
+    // expected input value is currently 27, so this allows some room to grow. change as needed if database column count increases. Also returns false if the length of the input is 0
+    if (input.length > 40 || input.length < 1) {
+        return false;
+    }
+    const validKeys = ["month_year", "name", "permit", "permit_recheck", "handgun", "long_gun", "other", "multiple", "admin", "prepawn_handgun", "prepawn_long_gun", "prepawn_other", "redemption_handgun", "redemption_long_gun", "redemption_other", "returned_handgun", "returned_long_gun", "returned_other", "rentals_handgun", "rentals_long_gun", "private_sale_handgun", "private_sale_long_gun", "private_sale_other", "return_to_seller_handgun","return_to_seller_long_gun", "return_to_seller_other","totals"];
+    for (elem in input) {
+       if(!validKeys.includes(elem))
+       {
+        return false;
+       }
+    }
+    return true;
+}
