7d191fb94bf88752bccb8fd200ff7ce87ada14e2
joelaspe <joe.laspe@gmail.com>
2023-06-16 14:10:29 -0700

    Added GET route by state abbreviation AND id. Returns specific id (one record)

--- server.js
+++ server.js
@@ -23,12 +23,34 @@ app.get('/checks', async (req,res) => {
     }
 });
 
-/**** GET ONE STATE BY ABBREVIATION -- returns all records for checks for that state*/ 
+/**** GET ONE STATE BY ABBREVIATION-- returns all records for checks for that state*/ 
 app.get('/checks/:state_abbv', async (req, res) => {
     const { state_abbv } = req.params;
     try {
         const result = await pool.query('SELECT states.name, states.abbreviation, checks.id, checks.month_year, checks.permit, checks.permit_recheck, checks.handgun, checks.long_gun, checks.other, checks.multiple, checks.admin, checks.prepawn_handgun, checks.prepawn_long_gun, checks.prepawn_other, checks.redemption_handgun, checks.redemption_long_gun, checks.redemption_other, checks.returned_handgun, checks.returned_long_gun, checks.returned_other, checks.rentals_handgun, checks.rentals_long_gun, checks.private_sale_handgun, checks.private_sale_long_gun, checks.private_sale_other, checks.return_to_seller_handgun, checks.return_to_seller_long_gun, checks.return_to_seller_other, checks.totals FROM checks INNER JOIN states ON checks.state_id = states.id WHERE states.abbreviation = $1 ORDER BY checks.month_year DESC', [state_abbv]);
-        res.status(200).json(result.rows);
+        if(result.rowCount < 1) {
+            res.status(404).send('Data Not Found. Check path is a valid state abbreviation: i.e. AK, AZ, NC, SD, etc');
+        } else {
+            res.status(200).json(result.rows);
+        }
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+/****** GET ONE CHECK BY ID, requires state abbreviation */
+app.get('/checks/:state_abbv/:id', async (req, res) => {
+    const { state_abbv } = req.params;
+    const { id } = req.params;
+    try {
+        const result = await pool.query('SELECT states.name, states.abbreviation, checks.id, checks.month_year, checks.permit, checks.permit_recheck, checks.handgun, checks.long_gun, checks.other, checks.multiple, checks.admin, checks.prepawn_handgun, checks.prepawn_long_gun, checks.prepawn_other, checks.redemption_handgun, checks.redemption_long_gun, checks.redemption_other, checks.returned_handgun, checks.returned_long_gun, checks.returned_other, checks.rentals_handgun, checks.rentals_long_gun, checks.private_sale_handgun, checks.private_sale_long_gun, checks.private_sale_other, checks.return_to_seller_handgun, checks.return_to_seller_long_gun, checks.return_to_seller_other, checks.totals FROM checks INNER JOIN states ON checks.state_id = states.id WHERE (states.abbreviation = $1) AND (checks.id = $2)', [state_abbv, id]);
+        if(result.rowCount < 1) {
+            res.status(404).send('Data Not Found. Check path is a valid state abbreviation: i.e. AK, AZ, NC, SD, etc. Ensure id passed is a valid number and corresponds to an id in the database.');
+        } else {
+            res.status(200).json(result.rows[0]);
+        }
     } catch (error) {
         console.error(error);
         res.status(500).send('Internal Server Error');
