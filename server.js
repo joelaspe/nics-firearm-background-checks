0faddb05cbd8301709b44abc02e95f2f1647fdad
joelaspe <joe.laspe@gmail.com>
2023-06-16 13:34:26 -0700

    Added basic GET route by state using abbreviation /checks/abbv

--- server.js
+++ server.js
@@ -23,6 +23,19 @@ app.get('/checks', async (req,res) => {
     }
 });
 
+/**** GET ONE STATE BY ABBREVIATION -- returns all records for checks for that state*/ 
+app.get('/checks/:state_abbv', async (req, res) => {
+    const { state_abbv } = req.params;
+    try {
+        const result = await pool.query('SELECT states.name, states.abbreviation, checks.id, checks.month_year, checks.permit, checks.permit_recheck, checks.handgun, checks.long_gun, checks.other, checks.multiple, checks.admin, checks.prepawn_handgun, checks.prepawn_long_gun, checks.prepawn_other, checks.redemption_handgun, checks.redemption_long_gun, checks.redemption_other, checks.returned_handgun, checks.returned_long_gun, checks.returned_other, checks.rentals_handgun, checks.rentals_long_gun, checks.private_sale_handgun, checks.private_sale_long_gun, checks.private_sale_other, checks.return_to_seller_handgun, checks.return_to_seller_long_gun, checks.return_to_seller_other, checks.totals FROM checks INNER JOIN states ON checks.state_id = states.id WHERE states.abbreviation = $1 ORDER BY checks.month_year DESC', [state_abbv]);
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+
 app.listen(process.env.PORT, () => {
     console.log(`Server started, listening on port: ${process.env.PORT}`);
 })
