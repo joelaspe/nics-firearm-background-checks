78fb8b452eec5a0b4f25c74ba4144664e2f56aab
joelaspe <joe.laspe@gmail.com>
2023-06-16 17:35:43 -0700

    Completed data validation for the POST route. No more status 500 should be occurring as long as the database is working.

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const BAD_DATA_ERROR = 'Bad Data, unable to create record. Ensure data is in json format. Example: {"month_year":"2023-06-01", "name":"Alabama", "permit":11111, "permit_recheck":2222, "handgun":3333, "long_gun":4444, "other":5555, "multiple":6666, "admin":7777, "prepawn_handgun":8888, "prepawn_long_gun":9999, "prepawn_other":12323, "redemption_handgun":3445, "redemption_long_gun":445667, "redemption_other":55678, "returned_handgun":78999, "returned_long_gun":11111, "returned_other":22222, "rentals_handgun":33333, "rentals_long_gun":44444, "private_sale_handgun":55555, "private_sale_long_gun":66666, "private_sale_other":77777, "return_to_seller_handgun":88888,"return_to_seller_long_gun":9999, "return_to_seller_other":12345,"totals":999999}';
+
 const express = require('express');
 const app = express(); // gives us access to methods and properties within express module
 
@@ -8,7 +10,10 @@ const { Pool } = require('pg');
 // create a new pool instance
 const pool = new Pool ({ connectionString: process.env.DATABASE_URL });
 
+
 app.use(express.json()); // middleware to allow us to read JSON body from HTTP requests
+
+
 
 /************** ROUTES  *****************/
 
@@ -60,9 +65,8 @@ app.get('/checks/:state_abbv/:id', async (req, res) => {
 /******** POST ONE CHECK, requires optional body data */
 app.post('/checks',  async (req, res) => {
     const body = req.body;
-    if(!checkValidInputKey(body)) {
-        res.status(400).send('Bad Data, unable to create record. Ensure data is in json format. Example: {"month_year":"2023-06-01", "name":"Alabama", "permit":11111, "permit_recheck":2222, "handgun":3333, "long_gun":4444, "other":5555, "multiple":6666, "admin":7777, "prepawn_handgun":8888, "prepawn_long_gun":9999, "prepawn_other":12323, "redemption_handgun":3445, "redemption_long_gun":445667, "redemption_other":55678, "returned_handgun":78999, "returned_long_gun":11111, "returned_other":22222, "rentals_handgun":33333, "rentals_long_gun":44444, "private_sale_handgun":55555, "private_sale_long_gun":66666, "private_sale_other":77777, "return_to_seller_handgun":88888,"return_to_seller_long_gun":9999, "return_to_seller_other":12345,"totals":999999}');
-    
+     if(!checkValidInput(body)) {
+        res.status(400).send(BAD_DATA_ERROR);
     }
     else {
         try {
@@ -71,11 +75,19 @@ app.post('/checks',  async (req, res) => {
             res.status(201).json(result.rows[0]);
         } catch (error) {
             console.error(error);
-            res.status(500).send('Internal Server Error, unable to create record. Ensure data is in json format. Example: {"month_year":"2023-06-01", "name":"Alabama", "permit":11111, "permit_recheck":2222, "handgun":3333, "long_gun":4444, "other":5555, "multiple":6666, "admin":7777, "prepawn_handgun":8888, "prepawn_long_gun":9999, "prepawn_other":12323, "redemption_handgun":3445, "redemption_long_gun":445667, "redemption_other":55678, "returned_handgun":78999, "returned_long_gun":11111, "returned_other":22222, "rentals_handgun":33333, "rentals_long_gun":44444, "private_sale_handgun":55555, "private_sale_long_gun":66666, "private_sale_other":77777, "return_to_seller_handgun":88888,"return_to_seller_long_gun":9999, "return_to_seller_other":12345,"totals":999999}');
+            res.status(500).send('Internal Server Error');
         }
     }
 });
 
+// error handler for when express JSON parser detects an error in the JSON body. 
+app.use((err, req, res, next) => {
+    if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+        console.error(err);
+        return res.status(400).send(`Bad Data. Error Message: ${err.message } `); // Bad request
+    }
+    next();
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server started, listening on port: ${process.env.PORT}`);
@@ -83,19 +95,31 @@ app.listen(process.env.PORT, () => {
 
 
 /**** Keyname data validation function 
- **** Returns false if a key in the object is not a valid one, otherwise returns true
+ **** Returns false if an entry in the object is not a valid one, otherwise returns true
 *****/
-function checkValidInputKey(input) {
-    // expected input value is currently 27, so this allows some room to grow. change as needed if database column count increases. Also returns false if the length of the input is 0
+function checkValidInput(input) {
+    // expected input length value is currently 27, so this allows some room to grow. change as needed if database column count increases. Also returns false if the length of the input is 0
     if (input.length > 40 || input.length < 1) {
         return false;
     }
     const validKeys = ["month_year", "name", "permit", "permit_recheck", "handgun", "long_gun", "other", "multiple", "admin", "prepawn_handgun", "prepawn_long_gun", "prepawn_other", "redemption_handgun", "redemption_long_gun", "redemption_other", "returned_handgun", "returned_long_gun", "returned_other", "rentals_handgun", "rentals_long_gun", "private_sale_handgun", "private_sale_long_gun", "private_sale_other", "return_to_seller_handgun","return_to_seller_long_gun", "return_to_seller_other","totals"];
+
     for (elem in input) {
-       if(!validKeys.includes(elem))
+        // check if keyname of input is a valid one
+        if(!validKeys.includes(elem))
        {
-        return false;
+            return false;
        }
+       // strings required for month_year and name
+       if(elem === 'month_year' || elem === 'name') {
+            if(typeof input[elem] != 'string') {
+                return false;
+            }
+            // all other entries must be integers
+        } else if(isNaN(parseInt(input[elem]))) {
+                return false;
+        }
     }
     return true;
 }
+
