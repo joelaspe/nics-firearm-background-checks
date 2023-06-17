2ee8541cf23234c43e8871bef5d572176879daae
joelaspe <joe.laspe@gmail.com>
2023-06-16 18:02:25 -0700

    Fixed bug where server crashed when a string was entered as an id on the GET /checks/state/id route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const BAD_DATA_ERROR = 'Bad Data, unable to create record. Ensure data is in json format. Example: {"month_year":"2023-06-01", "name":"Alabama", "permit":11111, "permit_recheck":2222, "handgun":3333, "long_gun":4444, "other":5555, "multiple":6666, "admin":7777, "prepawn_handgun":8888, "prepawn_long_gun":9999, "prepawn_other":12323, "redemption_handgun":3445, "redemption_long_gun":445667, "redemption_other":55678, "returned_handgun":78999, "returned_long_gun":11111, "returned_other":22222, "rentals_handgun":33333, "rentals_long_gun":44444, "private_sale_handgun":55555, "private_sale_long_gun":66666, "private_sale_other":77777, "return_to_seller_handgun":88888,"return_to_seller_long_gun":9999, "return_to_seller_other":12345,"totals":999999}';
+const BAD_ABBREVIATION_AND_ID = 'Data Not Found. Check path is a valid state abbreviation: i.e. AK, AZ, NC, SD, etc. Ensure id passed is a valid number and corresponds to an id in the database.'
 
 const express = require('express');
 const app = express(); // gives us access to methods and properties within express module
@@ -49,17 +50,22 @@ app.get('/checks/:state_abbv', async (req, res) => {
 app.get('/checks/:state_abbv/:id', async (req, res) => {
     const { state_abbv } = req.params;
     const { id } = req.params;
-    try {
-        const result = await pool.query('SELECT states.name, states.abbreviation, checks.id, checks.month_year, checks.permit, checks.permit_recheck, checks.handgun, checks.long_gun, checks.other, checks.multiple, checks.admin, checks.prepawn_handgun, checks.prepawn_long_gun, checks.prepawn_other, checks.redemption_handgun, checks.redemption_long_gun, checks.redemption_other, checks.returned_handgun, checks.returned_long_gun, checks.returned_other, checks.rentals_handgun, checks.rentals_long_gun, checks.private_sale_handgun, checks.private_sale_long_gun, checks.private_sale_other, checks.return_to_seller_handgun, checks.return_to_seller_long_gun, checks.return_to_seller_other, checks.totals FROM checks INNER JOIN states ON checks.state_id = states.id WHERE (states.abbreviation = $1) AND (checks.id = $2)', [state_abbv, id]);
-        if(result.rowCount < 1) {
-            res.status(404).send('Data Not Found. Check path is a valid state abbreviation: i.e. AK, AZ, NC, SD, etc. Ensure id passed is a valid number and corresponds to an id in the database.');
-        } else {
-            res.status(200).json(result.rows[0]);
+    if(isNaN(parseInt(id))) {
+        res.status(400).send(BAD_ABBREVIATION_AND_ID)
+    } else {
+        try {
+            const result = await pool.query('SELECT states.name, states.abbreviation, checks.id, checks.month_year, checks.permit, checks.permit_recheck, checks.handgun, checks.long_gun, checks.other, checks.multiple, checks.admin, checks.prepawn_handgun, checks.prepawn_long_gun, checks.prepawn_other, checks.redemption_handgun, checks.redemption_long_gun, checks.redemption_other, checks.returned_handgun, checks.returned_long_gun, checks.returned_other, checks.rentals_handgun, checks.rentals_long_gun, checks.private_sale_handgun, checks.private_sale_long_gun, checks.private_sale_other, checks.return_to_seller_handgun, checks.return_to_seller_long_gun, checks.return_to_seller_other, checks.totals FROM checks INNER JOIN states ON checks.state_id = states.id WHERE (states.abbreviation = $1) AND (checks.id = $2)', [state_abbv, id]);
+            if(result.rowCount < 1) {
+                res.status(404).send(BAD_ABBREVIATION_AND_ID);
+            } else {
+                res.status(200).json(result.rows[0]);
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).send('Internal Server Error');
         }
-    } catch (error) {
-        console.error(error);
-        res.status(500).send('Internal Server Error');
     }
+   
 });
 
 /******** POST ONE CHECK, requires optional body data */
@@ -79,6 +85,19 @@ app.post('/checks',  async (req, res) => {
         }
     }
 });
+
+/******** DELETE ONE CHECK, requires just an id on the route */
+app.delete('/checks/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+}); 
+
+
 
 // error handler for when express JSON parser detects an error in the JSON body. 
 app.use((err, req, res, next) => {
