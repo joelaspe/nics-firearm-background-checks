6b563505ebfb513e6e73c0ffd60aff7262fd58bb
joelaspe <joe.laspe@gmail.com>
2023-06-20 19:21:27 -0700

    Added POST (new record) capability

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,5 +1,96 @@
 const API_URL = 'https://nics-firearm-background-check-service.onrender.com/';
 let lastStatePage; // used for the cancel button, allows us to return back to the state page when hitting the button
+let lastState; // used for submission of new records, updated on the state page
+
+async function postData(e) {
+    const inputMonthYear = document.querySelector("#input-month-year");
+    const inputPermits = document.querySelector("#input-permits");
+    const inputHandgun = document.querySelector("#input-handgun");
+    const inputLongGun = document.querySelector("#input-long-gun");
+    const inputTotals = document.querySelector("#input-totals");
+    const requestOptions =  {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify( { "month_year": inputMonthYear.value, "permit": inputPermits.value, "handgun": inputHandgun.value, "long_gun": inputLongGun.value, "totals": inputTotals.value, "name": lastState })
+    };
+    const apiString = API_URL + 'checks/';
+    const response = await fetch(apiString, requestOptions);
+    const data = await response.json();
+
+    const submitBtn = document.querySelector("#submit-btn");
+    submitBtn.style.display = "none";
+    submitBtn.onclick = putData;
+
+    const tableBody = document.querySelector("#checks-table-body");
+    const successMsg = document.createElement("h2");
+    successMsg.classList.add("success-msg");
+    successMsg.id = "success-msg";
+    successMsg.textContent = 'Record created'
+    tableBody.appendChild(successMsg);
+
+
+
+}
+
+function newRecord() {
+    const homeBtn = document.querySelector("#home-btn");
+    homeBtn.style.display = "none";
+    const newBtn = document.querySelector("#new-btn");
+    newBtn.style.display = "none";
+    const cancelBtn = document.querySelector("#cancel-btn");
+    cancelBtn.style.display = "inline-block";
+    cancelBtn.onclick = getStateData;
+    const deleteBtn = document.querySelector("#delete-btn");
+    deleteBtn.style.display = "none";
+    deleteBtn.onclick = deleteRecord;
+    const submitBtn = document.querySelector("#submit-btn");
+    submitBtn.style.display = "inline-block";
+    submitBtn.onclick = postData;
+    
+    // clear the table
+    const tableBody = document.querySelector("#checks-table-body");
+    tableBody.replaceChildren();
+
+    const inputMonthYear = document.createElement("input");
+    inputMonthYear.id = "input-month-year";
+    const labelMonthYear = document.createElement("label");
+    labelMonthYear.htmlFor = "input-month-year";
+    labelMonthYear.textContent = "Year/Month";
+    tableBody.appendChild(labelMonthYear);
+    tableBody.appendChild(inputMonthYear);
+
+    const inputPermits = document.createElement("input");
+    inputPermits.id = "input-permits";
+    const labelPermits = document.createElement("label");
+    labelPermits.htmlFor = "input-permits";
+    labelPermits.textContent = "Permits";
+    tableBody.appendChild(labelPermits);
+    tableBody.appendChild(inputPermits);
+
+    const inputHandgun = document.createElement("input");
+    inputHandgun.id = "input-handgun";
+    const labelHandgun = document.createElement("label");
+    labelHandgun.htmlFor = "input-handgun";
+    labelHandgun.textContent = "Handguns"
+    tableBody.appendChild(labelHandgun);
+    tableBody.appendChild(inputHandgun);
+
+    const inputLongGun = document.createElement("input");
+    inputLongGun.id = "input-long-gun";
+    const labelLongGun = document.createElement("label");
+    labelLongGun.htmlFor = "input-long-gun";
+    labelLongGun.textContent = "Long Guns";
+    tableBody.appendChild(labelLongGun);
+    tableBody.appendChild(inputLongGun);
+    
+    const inputTotals = document.createElement("input");
+    inputTotals.id = "input-totals";
+    const labelTotals = document.createElement("label");
+    labelTotals.htmlFor = "input-totals";
+    labelTotals.textContent = "Totals";
+    tableBody.appendChild(labelTotals);
+    tableBody.appendChild(inputTotals);
+}
 
 async function putData() {
     const inputId = document.querySelector("#input-id");
@@ -24,18 +115,47 @@ async function putData() {
     successMsg.textContent = 'Record updated'
     tableBody.appendChild(successMsg);
 }
+async function deleteRecord() {
+    const inputId = document.querySelector("#input-id");
+    const requestOptions = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' 
+    }};
+    const apiString = API_URL + 'checks/' + inputId.value + '/';
+    const response = await fetch(apiString, requestOptions)
+    const data = await response.json();
+    
+    const deleteBtn = document.querySelector("#delete-btn");
+    deleteBtn.style.display = "none";
+    const submitBtn = document.querySelector("#submit-btn");
+    deleteBtn.style.display = "none";
+
+    const tableBody = document.querySelector("#checks-table-body");
+    const successMsg = document.createElement("h2");
+    successMsg.classList.add("success-msg");
+    successMsg.id = "success-msg";
+    successMsg.textContent = 'Record deleted'
+    tableBody.replaceChildren(successMsg);
+
+}
+
 
 /****** generateCheckForm creates the form for updating a record */
 function generateCheckForm(data) {
+    const homeBtn = document.querySelector("#home-btn");
+    homeBtn.style.display = "none";
+    const newBtn = document.querySelector("#new-btn");
+    newBtn.style.display = "none";
     const cancelBtn = document.querySelector("#cancel-btn");
     cancelBtn.style.display = "inline-block";
     cancelBtn.onclick = getStateData;
+    const deleteBtn = document.querySelector("#delete-btn");
+    deleteBtn.style.display = "inline-block";
+    deleteBtn.onclick = deleteRecord;
     const submitBtn = document.querySelector("#submit-btn");
     submitBtn.style.display = "inline-block";
     submitBtn.onclick = putData;
     
-    console.log(data);
-    lastState = data.name;
     // clear the table
     const tableBody = document.querySelector("#checks-table-body");
     tableBody.replaceChildren();
@@ -54,7 +174,7 @@ function generateCheckForm(data) {
     inputMonthYear.id = "input-month-year";
     const labelMonthYear = document.createElement("label");
     labelMonthYear.htmlFor = "input-month-year";
-    labelMonthYear.textContent = "Month/Year";
+    labelMonthYear.textContent = "Year/Month";
     tableBody.appendChild(labelMonthYear);
     tableBody.appendChild(inputMonthYear);
 
@@ -106,7 +226,6 @@ async function editCheck(e) {
     // parse out the check id from the id name
     const checkId = tr.id.slice(6);
     const apiString = API_URL + 'check/' + checkId + '/';
-    console.log(apiString);
     const response = await fetch(apiString);
     const data = await response.json();
     generateCheckForm(data);
@@ -120,11 +239,17 @@ function generateStateTable(data) {
     const stateImage = document.querySelector("#state-image");
     stateImage.src = `images/maps/${abbreviation}.jpg`;
     stateImage.alt = `Outline of ${data[0].name}`;
-    
+    lastState = data[0].name;
+
     const homeBtn = document.querySelector("#home-btn");
     homeBtn.style.display = "inline-block";
+    const newBtn = document.querySelector("#new-btn");
+    newBtn.style.display = "inline-block";
+    newBtn.onclick = newRecord;
     const cancelBtn = document.querySelector("#cancel-btn");
     cancelBtn.style.display = "none";
+    const deleteBtn = document.querySelector("#delete-btn");
+    deleteBtn.style.display = "none";
     const submitBtn = document.querySelector("#submit-btn");
     submitBtn.style.display = "none";
     
@@ -152,7 +277,7 @@ function generateStateTable(data) {
     }
        
     // create table header
-    const headers = ['Month/Year', 'Permits', 'Handgun Purchase', 'Long Gun Purchase', 'Total Checks'];
+    const headers = ['Year/Month', 'Permits', 'Handgun Purchase', 'Long Gun Purchase', 'Total Checks'];
     const tableHead = document.createElement("tr");
     for(let i = 0; i < headers.length; i++) {
         const th = document.createElement("th");
@@ -215,7 +340,6 @@ async function getStateData(e) {
     // parse out the abbreviation from the id name
     const abbreviation = tr.id.slice(3);
     const apiString = API_URL + 'checks/' + abbreviation + '/';
-    console.log(apiString);
     const response = await fetch(apiString);
     const data = await response.json();
     generateStateTable(data);
@@ -230,8 +354,12 @@ function generateMonthTable(data) {
 
     const homeBtn = document.querySelector("#home-btn");
     homeBtn.style.display = "none";
+    const newBtn = document.querySelector("#new-btn");
+    newBtn.style.display = "none";
     const cancelBtn = document.querySelector("#cancel-btn");
     cancelBtn.style.display = "none";
+    const deleteBtn = document.querySelector("#delete-btn");
+    deleteBtn.style.display = "none";
     const submitBtn = document.querySelector("#submit-btn");
     submitBtn.style.display = "none";
 
